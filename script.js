f63f002438ebda6eabc186da3b475b87fd339e4a
JacobMarineau <jacobmarineaump@gmail.com>
2024-08-30 15:48:10 -0500

    Grab values work now

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-function grabValues() {}
+function grabValues(event) {
+  event.preventDefault();
+  const fName = document.getElementById("f-Name").value;
+  const lname = document.getElementById("l-Name").value;
+  const id = document.getElementById("id").value;
+  const title = document.getElementById("title").value;
+  const salary = document.getElementById("an-sal").value;
+
+  const tbody = document.getElementById("t-body");
+  const newT = document.createElement("tr");
+  newT.classList.add("t-body");
+  tbody.appendChild(newT);
+  newT.innerHTML = `  
+  <td>${fName}</td>
+  <td>${lname}</td>
+   <td>${id}</td>
+    <td>${title}</td>
+     <td>${salary}</td>
+      `;
+}
