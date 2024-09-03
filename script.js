84063be79bc9518619cd128a3021cbdb597322d5
JacobMarineau <jacobmarineaump@gmail.com>
2024-09-03 09:33:54 -0500

    Updated code so that the values become empty after submitting

--- script.js
+++ script.js
@@ -4,26 +4,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function grabValues(event) {
   event.preventDefault();
-  const fName = document.getElementById("f-Name").value;
-  const lname = document.getElementById("l-Name").value;
-  const id = document.getElementById("id").value;
-  const title = document.getElementById("title").value;
-  const salary = document.getElementById("an-sal").value;
+  const fNameValue = document.getElementById("f-Name").value;
+  const lNameValue = document.getElementById("l-Name").value;
+  const idValue = document.getElementById("id").value;
+  const titleValue = document.getElementById("title").value;
+  const salaryValue = document.getElementById("an-sal").value;
+  let fName = document.getElementById("f-Name");
+  let lName = document.getElementById("l-Name");
+  let id = document.getElementById("id");
+  let title = document.getElementById("title");
+  let salary = document.getElementById("an-sal");
 
   const tbody = document.getElementById("t-body");
   const newT = document.createElement("tr");
   newT.classList.add("tdata");
   tbody.appendChild(newT);
   newT.innerHTML = `  
-  <td>${fName}</td>
-  <td>${lname}</td>
-   <td>${id}</td>
-    <td>${title}</td>
-     <td>${salary}</td>
+  <td>${fNameValue}</td>
+  <td>${lNameValue}</td>
+   <td>${idValue}</td>
+    <td>${titleValue}</td>
+     <td>${salaryValue}</td>
      <td>
      <button class="remButt" onClick="removeRow(event)">Remove</button>
      </td>
       `;
+  resetValues();
+}
+
+function resetValues() {
+  document.getElementById("f-Name").value = "";
+  document.getElementById("l-Name").value = "";
+  document.getElementById("id").value = "";
+  document.getElementById("title").value = "";
+  document.getElementById("an-sal").value = "";
 }
 
 function removeRow(event) {
